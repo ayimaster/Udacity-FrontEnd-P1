61597a2bb2729f152be71af8c992b0baedd2f29e
Ayima Okeeva <ayimaster@gmail.com>
2015-09-29 06:59:08 +0200

    Removed whitespaces Fixed margins Created container for the content Changed the hovering color over a links Fixed indentation

---
+++ hamburger.js
@@ -0,0 +1,9 @@
+/*http://www.internetkultur.at/simple-hamburger-drop-down-menu-with-css-and-jquery/*/
+
+jQuery(document).ready(function($){
+    jQuery(function ($) {
+      $('.menu-btn').click(function () {
+        $('.responsive-menu').toggleClass('expand')
+      })
+    })
+});
